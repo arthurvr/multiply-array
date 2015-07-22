24157019f3b0565875365c97f91f438138d451ef
Sultan Imanhodjaev <sultan.imanhodjaev@gmail.com>
2015-07-22 18:26:29 +0600

    Made changes as @arthurvr @kevva advised

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 'use strict';
 module.exports = function(arr) {
-	var product = 1;
+	var product;
 
 	arr = Array.isArray(arr) ? arr : arguments;
+	product = arr[0]
 
-	if (arr.length >= 2) {
-		for (var i = 0; i < arr.length; i++) {
-			product *= arr[i];
-		}
-
-		return product;
-	} else {
+	if (arr.length < 2) {
 		throw new Error('The array should at least have 2 items');
 	}
+
+	for (var i = 1; i < arr.length; i++) {
+		product *= arr[i];
+	}
+
+	return product;	
 };
