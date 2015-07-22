2fcfd494accf42c25a0bdff118ad573c46d20525
Sultan Imanhodjaev <sultan.imanhodjaev@gmail.com>
2015-07-22 18:30:27 +0600

    Remove accepting

--- index.js
+++ index.js
@@ -2,12 +2,15 @@
 module.exports = function(arr) {
 	var product;
 
-	arr = Array.isArray(arr) ? arr : arguments;
-	product = arr[0];
+	if (false === Array.isArray(arr)) {
+		throw new Error('Please supply an array of integers');
+	}
 
 	if (arr.length < 2) {
 		throw new Error('The array should at least have 2 items');
 	}
+
+	product = arr[0];
 
 	for (var i = 1; i < arr.length; i++) {
 		product *= arr[i];
